19b25237700f2ee63e34bdec6fd3ed4a3a898ae6
muham <muhambriana.31@gmail.com>
2025-04-28 21:05:28 +0700

    day2: add background color with modifier

--- app/src/main/java/com/mshell/learncompose/MainActivity.kt
+++ app/src/main/java/com/mshell/learncompose/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -18,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -68,6 +70,7 @@ fun GreetingText(
             modifier = Modifier
                 .padding(16.dp)
                 .align(alignment = Alignment.End)
+                .background(color = Color.Blue)
         )
     }
 }
