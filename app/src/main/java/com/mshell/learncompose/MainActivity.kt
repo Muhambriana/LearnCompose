5116965f8162bfa738e7e029449d1b1f7b077f88
muham <muhambriana.31@gmail.com>
2025-06-15 21:46:31 +0700

    day1: Creating columns and rows. And add button

--- app/src/main/java/com/mshell/learncompose/MainActivity.kt
+++ app/src/main/java/com/mshell/learncompose/MainActivity.kt
@@ -8,7 +8,9 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -35,26 +37,51 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyApp(modifier: Modifier = Modifier) {
-    Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.primary
-    ) {
-        Greeting("Android")
+fun MyApp(
+    modifier: Modifier = Modifier,
+    names: List<String> = listOf("World", "Compose")
+) {
+    Column(modifier = modifier) {
+        for (name in names) {
+            Greeting(name)
+        }
     }
 }
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.padding(24.dp)
-        )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = modifier
+                .padding(24.dp)
+        ) {
+            Column(
+                modifier = modifier.weight(1f)
+            ) {
+                Text(
+                    text = "Hello",
+                )
+                Text(
+                    text = name,
+                )
+            }
+            ElevatedButton(
+                onClick = {}
+            ) {
+                Text("Show more")
+            }
+        }
     }
 }
 
-@Preview(showBackground = true, name = "Text preview")
+@Preview(
+    showBackground = true,
+    name = "Text preview",
+    widthDp = 320
+)
 @Composable
 fun GreetingPreview() {
     LearnComposeTheme {
