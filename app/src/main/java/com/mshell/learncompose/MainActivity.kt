f2f07a8ec6c5c9debc085e47151a27fd5c2a9156
muham <muhambriana.31@gmail.com>
2025-06-16 20:17:57 +0700

    day2: state in compose

--- app/src/main/java/com/mshell/learncompose/MainActivity.kt
+++ app/src/main/java/com/mshell/learncompose/MainActivity.kt
@@ -16,6 +16,8 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
@@ -50,16 +52,19 @@ fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    val expanded = remember { mutableStateOf(false) }
+    val extraPadding = if (expanded.value) 48.dp else 0.dp
+
     Surface(
         color = MaterialTheme.colorScheme.primary,
         modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
         Row(
-            modifier = modifier
+            modifier = Modifier
                 .padding(24.dp)
         ) {
             Column(
-                modifier = modifier.weight(1f)
+                modifier = Modifier.weight(1f).padding(bottom = extraPadding)
             ) {
                 Text(
                     text = "Hello",
@@ -69,9 +74,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
                 )
             }
             ElevatedButton(
-                onClick = {}
+                onClick = { expanded.value = !expanded.value }
             ) {
-                Text("Show more")
+                Text(
+                    if (expanded.value) "Show less"
+                    else "Show more"
+                )
             }
         }
     }
