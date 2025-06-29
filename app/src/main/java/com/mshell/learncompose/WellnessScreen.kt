613545e6aecac4179e9568077bfca26bc1e68077
muham <muhambriana.31@gmail.com>
2025-06-29 21:06:24 +0700

    day1: Events in Compose

--- app/src/main/java/com/mshell/learncompose/WellnessScreen.kt
+++ app/src/main/java/com/mshell/learncompose/WellnessScreen.kt
@@ -2,8 +2,15 @@ package com.mshell.learncompose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
     WaterCounter(modifier)
+}
+
+@Preview(showBackground = true)
+@Composable
+fun WellnessPreview() {
+    WellnessScreen()
 }
