c7dc6e3079ff28e9a522b13c0acb1f7a27d414a2
muham <muhambriana.31@gmail.com>
2025-06-29 21:21:37 +0700

    day1: Memory in a composable function

--- app/src/main/java/com/mshell/learncompose/WaterCounter.kt
+++ app/src/main/java/com/mshell/learncompose/WaterCounter.kt
@@ -6,6 +6,12 @@ import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.material3.Text
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -14,7 +20,8 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(16.dp)
     ) {
-        var count = 0
+        var count by remember { mutableIntStateOf(0) }
+
         Text(
             text = "You've had $count glasses."
         )
