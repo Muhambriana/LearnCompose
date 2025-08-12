46bd786eac7501d02aee7d5c9c4855fa3d2e6f17
muham <muhambriana.31@gmail.com>
2025-08-12 15:01:45 +0700

    day2: restore state in compose (when rotate or chang theme or change language)

--- app/src/main/java/com/mshell/learncompose/WaterCounter.kt
+++ app/src/main/java/com/mshell/learncompose/WaterCounter.kt
@@ -11,6 +11,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -20,7 +21,7 @@ fun WaterCounter(modifier: Modifier = Modifier) {
     Column(
         modifier = modifier.padding(16.dp)
     ) {
-        var count by remember { mutableIntStateOf(0) }
+        var count by rememberSaveable { mutableIntStateOf(0) }
 
         if (count > 0) {
             Text(text = "You've had $count glasses.")
