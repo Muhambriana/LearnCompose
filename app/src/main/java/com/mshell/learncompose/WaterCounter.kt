133cc540946d4a4c4955ff6e0f5b854a35f5a654
muham <muhambriana.31@gmail.com>
2025-08-12 14:53:31 +0700

    rollback code

--- app/src/main/java/com/mshell/learncompose/WaterCounter.kt
+++ app/src/main/java/com/mshell/learncompose/WaterCounter.kt
@@ -1,7 +1,6 @@
 package com.mshell.learncompose
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
@@ -24,29 +23,14 @@ fun WaterCounter(modifier: Modifier = Modifier) {
         var count by remember { mutableIntStateOf(0) }
 
         if (count > 0) {
-            var showTask by remember { mutableStateOf(true) }
-            if (showTask) {
-                WellnessTaskItem(
-                    onClose = { showTask = false },
-                    taskName = "Have you taken your 15 minute walk today?"
-                )
-            }
             Text(text = "You've had $count glasses.")
         }
-
-        Row(Modifier.padding(top = 8.dp)) {
-            Button(
-                onClick = { count++ },
-                enabled = count < 10
-            ) {
-                Text("Add one")
-            }
-            Button(
-                onClick = { count = 0 },
-                Modifier.padding(start = 8.dp)
-            ) {
-                Text("Clear water count")
-            }
+        Button(
+            onClick = { count++ },
+            modifier = Modifier.padding(top = 8.dp),
+            enabled = count < 10
+        ) {
+            Text("Add one")
         }
     }
 }
