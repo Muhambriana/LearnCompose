39f62cae1309729d228e348cd595914293f575ce
muham <muhambriana.31@gmail.com>
2025-08-13 13:27:32 +0700

    day3: work with lists

--- app/src/main/java/com/mshell/learncompose/WellnessScreen.kt
+++ app/src/main/java/com/mshell/learncompose/WellnessScreen.kt
@@ -1,12 +1,16 @@
 package com.mshell.learncompose
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WellnessScreen(modifier: Modifier = Modifier) {
-    WaterCounter(modifier)
+    Column(modifier = modifier) {
+        StatefulCounter()
+        WellnessTasksList()
+    }
 }
 
 @Preview(showBackground = true)
